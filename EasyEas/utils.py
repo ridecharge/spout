491dfa5e3da8402a402b1a0b3b1155abec264638
Alex Freas <akfreas@gmail.com>
2012-08-29 12:47:12 -0400

    Changed file upload method to pull version from plist and name file accordingly.

--- EasyEas/utils.py
+++ EasyEas/utils.py
@@ -1,4 +1,9 @@
 from django.conf import settings
+from zipfile import ZipFile
+import shutil
+import os
+from Foundation import NSDictionary, NSAutoreleasePool
+
 
 
 def ipa_path(app_name, version):
@@ -7,17 +12,47 @@ def ipa_path(app_name, version):
     return path
 
 
-def save_uploaded_file(the_file, version):
+def save_uploaded_file(the_file):
 
-    split_filename = the_file.name.split('.')
+    
+    temp_file_path = "/tmp/temp_file.ipa"
+    temp_file = open(temp_file_path, "w")
 
-    split_filename.insert(-1, "-" + version)
-    split_filename.insert(-1, ".")
+    for chunk in the_file.chunks():
+        temp_file.write(chunk)
+    temp_file.close()
+
+    import re
+    regex = re.compile(".*\.app/$")
+    ipa_contents = ZipFile(temp_file_path).filelist
+
+    app_name = "".join([thefile.filename for thefile in ipa_contents if regex.match(thefile.filename)][0].split(".")[0:-1][0].split("/")[-1])
+
+    ipa_plist = plist_from_ipa(temp_file_path, app_name)
+    version = ipa_plist['CFBundleVersion']
 
 
-    new_file_location = settings.STATIC_ROOT + "/" + "".join(split_filename)
-    new_file = open(new_file_location, "w") 
+    new_file_location = "%s/%s-%s.ipa" % (settings.STATIC_ROOT, app_name, version)
 
-    for c in the_file.chunks():
-        new_file.write(c)
-    new_file.close()
+    shutil.move(temp_file_path, new_file_location)
+
+    return new_file_location
+
+def plist_from_ipa(filename, app_name):
+
+    thezip = ZipFile(filename)
+    plist_dict = thezip.open("Payload/" + app_name + ".app/" + "Info.plist")
+
+    tempfile = open("/tmp/temp_plist.plist", "w")
+
+    for line in plist_dict:
+        tempfile.write(line)
+
+    tempfile.close()
+
+    pool = NSAutoreleasePool.alloc().init()
+    parsed_dict = NSDictionary.dictionaryWithContentsOfFile_("/tmp/temp_plist.plist")
+    copied_dict = dict(parsed_dict)
+
+    return copied_dict
+ 
