67a3c6b11ac9ca48f338c6cd8b195ed244056595
Alex Freas <akfreas@gmail.com>
2012-08-29 12:46:25 -0400

    Removed version from form, not needed since pulling from bundle.

--- EasyEas/forms.py
+++ EasyEas/forms.py
@@ -5,6 +5,5 @@ class UploadBuildForm(forms.Form):
 
 
     note = forms.CharField(max_length=255)
-    version = forms.CharField(max_length=50)
     file = forms.FileField()
     product = forms.ModelChoiceField(models.Product.objects.all())
