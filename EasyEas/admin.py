8347e240d54c0603d3da2b4e060388f5ed245dff
Alex Freas <akfreas@gmail.com>
2012-09-12 12:05:24 -0400

    Added tag to admin.

--- EasyEas/admin.py
+++ EasyEas/admin.py
@@ -12,5 +12,11 @@ admin.site.register(App, AppAdmin)
 class ProductAdmin(admin.ModelAdmin):
 
     list_display = ('name',)
-    
+
 admin.site.register(Product, ProductAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description',)
+
+admin.site.register(Tag, TagAdmin)    
