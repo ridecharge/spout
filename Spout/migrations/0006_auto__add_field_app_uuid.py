086300bc0ab5ba416c0df4bd852ef2d54d269ede
Alex Freas <akfreas@gmail.com>
2012-09-21 01:14:14 -0400

    Migration for uuid field in app model.

---
+++ Spout/migrations/0006_auto__add_field_app_uuid.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'App.uuid'
+        db.add_column('Spout_app', 'uuid',
+                      self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'App.uuid'
+        db.delete_column('Spout_app', 'uuid')
+
+
+    models = {
+        'Spout.app': {
+            'Meta': {'unique_together': "(('version', 'name'),)", 'object_name': 'App'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'comment': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'device_type': ('django.db.models.fields.CharField', [], {'default': "'IOS'", 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'note': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Spout.Product']"}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['Spout.Tag']", 'symmetrical': 'False'}),
+            'uuid': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'Spout.product': {
+            'Meta': {'object_name': 'Product'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'Spout.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['Spout']
