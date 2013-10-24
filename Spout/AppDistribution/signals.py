ef27cdde3700d2595d1ccc76c8bbd2fd9f7ccf83
Alex Freas <akfreas@gmail.com>
2013-10-23 21:52:12 -0400

    Added post save signal in signals.py and now removing specific app asset when on delete.

--- Spout/AppDistribution/signals.py
+++ Spout/AppDistribution/signals.py
@@ -2,15 +2,29 @@ from django.db.models.signals import pre_delete, pre_save, post_save, post_syncd
 
 from AppDistribution.models import *
 
+from AppDistribution.PackageHandlers import PackageHandler
 from django.contrib.auth import models as auth_app, get_user_model
 from django.contrib.auth.models import User
 from AppDistribution.models import *
+from datetime import datetime 
+
+
+
 
 def delete_app(sender, instance, signal, *args, **kwargs):
-    app_file = "%s/%s.ipa" % (settings.MEDIA_ROOT, instance.uuid)
     try:
-        remove(app_file)
+        for asset in instance.assets.all():
+            remove(asset.asset_file.name)
+
     except OSError:
         pass
 
+def save_app(sender, instance, signal, *args, **kwargs):
+
+    if instance.primary is True:
+        handler = PackageHandler(instance)
+        handler.handle()
+
+
+post_save.connect(save_app, sender=AppAsset)
 pre_delete.connect(delete_app, sender=App)
